73beb628be779d67413ec7df9dbaf7f78ac0fdec
thatotherguyfromclass <princeinioluwa04@gmail.com>
2025-07-04 12:44:35 -0700

    Updated code before going live

--- core/settings.py
+++ core/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'perfume_db',
+        'USER': 'perfume_user',
+        'PASSWORD': 'princegratung',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
